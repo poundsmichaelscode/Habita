0319db08ad11b67627463efead357e0effc00250
olayenikanmichael-Dev <olayenikanmichaelolaniyi@gmail.com>
2026-01-29 07:51:30 +0100

    property landlord page

---
+++ Frontend/habita/app/components/ContactButton.tsx
@@ -0,0 +1,8 @@
+
+const ContactButton = () => {
+  return (
+    <div className="mt-6 py-2 px-6 bg-habita cursor-pointer text-white rounded-xl hover:bg-blue-500 transition ">Contact</div>
+  )
+}
+
+export default ContactButton
---
+++ Frontend/habita/app/landlords/[id]/page.tsx
@@ -0,0 +1,50 @@
+import Image from "next/image";
+import ContactButton from "@/app/components/ContactButton";
+import Propertylist from "@/app/components/Properties/Propertylist";
+const LandLordDetailpage = () => {
+return (
+<>
+<main className = "max-w-[1500px] mx-auto px-6 pb-6">
+
+<div className = " grid grid-cols-1 md:grid-cols-4 gap-4">
+
+<aside className = "col-span-1 mb-4">
+
+<div className="flex flex-col items-center p-6 rounded-xl border-0 shadow-2xl">
+<Image 
+src="/profileimg.jpg"
+width={200}
+height={200}
+className="rounded-full"
+alt="landlord profile picture"/>
+
+<h1 className="mt-6 text-2xl">Landlord Name</h1>
+
+<ContactButton/> 
+
+
+
+
+</div>
+
+</aside>
+
+<div  className="col-span-3 pl-0 md:pl-6"> 
+  
+  <div className="grid grid-cols-1  md:grid-cols-3 lg:grid-cols- gap-6">
+ <Propertylist/>
+
+
+  </div>
+  
+  
+  
+
+</div>
+</div>
+</main>
+</>
+  )
+}
+
+export default LandLordDetailpage
