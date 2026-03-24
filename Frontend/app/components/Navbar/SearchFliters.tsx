a1a4fe910e032f229177853bd293b628ebf4a85b
olayenikanmichael-Dev <olayenikanmichaelolaniyi@gmail.com>
2026-03-24 14:28:22 +0100

    quick fix

--- Frontend/app/components/Navbar/SearchFliters.tsx
+++ Frontend/app/components/Navbar/SearchFliters.tsx
@@ -36,13 +36,21 @@ const SearchFilters = () => {
 
             <div className="p-2">
                 <div className="cursor-pointer p-2 lg:p-4 bg-airbnb hover:bg-airbnb-dark transition rounded-full text-white">
-                    <svg 
-                        viewBox="0 0 32 32" 
-                        style={{display:'block', fill:'none', height: '16px', width: '16px', stroke: 'currentColor', strokeWidth:4, overflow:'visible'}} 
-                        aria-hidden="true" role="presentation" focusable="false"
-                    >
-                        <path fill="none" d="M13 24a11 11 0 1 0 0-22 11 11 0 0 0 0 22zm8-3 9 9"></path>
-                    </svg>
+                 <svg 
+  viewBox="0 0 32 32" 
+  style={{
+    display: 'block',
+    fill: 'none',
+    height: '16px',
+    width: '16px',
+    stroke: 'blue', 
+    strokeWidth: 4
+  }}
+>
+  <path d="M13 24a11 11 0 1 0 0-22 11 11 0 0 0 0 22zm8-3 9 9" />
+</svg>
+  
+
                 </div>
             </div>
         </div>
